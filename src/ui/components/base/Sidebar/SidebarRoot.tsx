c8e527e62b5239af04d6821643334cddeda4c393
zougui1 <zougui.dev@protonmail.com>
2026-07-21 22:16:07 +0200

    colored shadows

--- src/ui/components/base/Sidebar/SidebarRoot.tsx
+++ src/ui/components/base/Sidebar/SidebarRoot.tsx
@@ -99,7 +99,8 @@ export function SidebarRoot({
           variant === "floating" || variant === "inset"
             ? `p-2
               group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4))+2px)]`
-            : `group-data-[collapsible=icon]:w-(--sidebar-width-icon)
+            : `border-border
+              group-data-[collapsible=icon]:w-(--sidebar-width-icon)
               group-data-[side=left]:border-r group-data-[side=right]:border-l`,
           className,
         )}
@@ -110,9 +111,8 @@ export function SidebarRoot({
           data-slot="sidebar-inner"
           className="bg-background
             group-data-[variant=floating]:border-sidebar-border flex h-full
-            w-full flex-col group-data-[variant=floating]:rounded-lg
-            group-data-[variant=floating]:border
-            group-data-[variant=floating]:shadow-sm"
+            w-full flex-col shadow-sm group-data-[variant=floating]:rounded-lg
+            group-data-[variant=floating]:border"
         >
           {children}
         </div>
